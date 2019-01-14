8db1e6858f74300383d8dc59ccbb6c1e4ade34b8
Kirill Shvakov <shvakov@gmail.com>
2019-01-14 22:07:55 +0200

    synv

--- cmd/proton/main.go
+++ cmd/proton/main.go
@@ -25,7 +25,7 @@ func init() {
 	flag.StringVar(&options.DSN, "dsn", "native://127.0.0.1:9000", "ClickHouse DSN")
 	flag.StringVar(&options.Address, "addr", ":30002", "listen address")
 	flag.StringVar(&options.MetricsAddress, "metrics_addr", ":2112", "address on which to expose metrics")
-	flag.IntVar(&options.BacklogSize, "backlog", 100000, "backlog size")
+	flag.IntVar(&options.BacklogSize, "backlog", 10000, "backlog size")
 	flag.IntVar(&options.Concurrency, "concurrency", 2, "number of the background processes")
 	flag.StringVar(&pprof, "pprof", "", "pprof address. If set to start the pprof server")
 }
